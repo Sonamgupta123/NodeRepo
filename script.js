99b0448c18bfc9b08f610127fdfda44c524f9847
Sonam Gupta <sonamflower34@gmail.com>
2026-03-16 18:21:50 +0000

    more concept in script.js

--- script.js
+++ script.js
@@ -6,8 +6,12 @@
 
 // process in node
 
-let args = process.argv;
+// let args = process.argv;
 
-for(let i=2 ;i<args.length ;i++){
-    console.log("hello to ",args[i]);
-}
+// for(let i=2 ;i<args.length ;i++){
+//     console.log("hello to ",args[i]);
+// }
+
+const someValue = require("./math");
+
+console.log(someValue);
